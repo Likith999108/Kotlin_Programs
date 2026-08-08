3d07faec20aa57c0265d231cb3e77ded20f71240
Likith Chowdary Yasangi <ourlikith@gmail.com>
2026-08-08 13:35:54 +0530

    Add MainActivity with Hello World display

---
+++ Hello.kt
@@ -0,0 +1,52 @@
+package com.example.a24b81a05u5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            Display()
+        }
+    }
+}
+
+@Composable
+fun Display() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello, World!",
+            color = Color.Red,
+            textAlign = TextAlign.Center,
+            fontSize = 36.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DisplayPreview() {
+    Display()
+}
